b93e551c9bc1d7284d4ff62c19d6a5e31563c2fa
robotics3738 <robotics3738@gmail.com>
2018-01-24 21:30:12 -0600

    Update package.

--- RnD_TestCode/ChainLiftTest.java
+++ RnD_TestCode/ChainLiftTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.OldCode;
+package org.firstinspires.ftc.teamcode.RnD_TestCode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
