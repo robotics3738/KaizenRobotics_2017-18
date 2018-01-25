572dc6ff249985274ec178130ea494f5feca20f0
robotics3738 <robotics3738@gmail.com>
2018-01-24 21:21:19 -0600

    Moved to appropriate folder.
    Removed duplicate legal notice. Cleaned up code and removed extra lines of code to make it a "Blank Template."

---
+++ Non_Competition_Code/BlankTemplate.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Disabled
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Blank Template", group="Unknown")
+public class BlankTemplate extends OpMode
+{
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        //CodeHere
+    }
+
+    @Override
+    public void stop() {
+    }
+}
