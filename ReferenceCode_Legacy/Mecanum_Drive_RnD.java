695caf03b36c727377f78193b1dc56336ab166b0
robotics3738 <robotics3738@gmail.com>
2018-01-24 21:32:01 -0600

    Update package.

--- ReferenceCode_Legacy/Mecanum_Drive_RnD.java
+++ ReferenceCode_Legacy/Mecanum_Drive_RnD.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.ReferenceCode_Legacy;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.robotcore.hardware.Servo;
